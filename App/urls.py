e1ade11df99554b83fec8bd86ea4d4f048ea6f3c
apigituser <responsemail850@gmail.com>
2025-08-25 15:24:51 +0530

    login path added

--- App/urls.py
+++ App/urls.py
@@ -1,7 +1,9 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('users/', views.ListUsers.as_view()),
     path('register/', views.UserRegistration.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
 ]
